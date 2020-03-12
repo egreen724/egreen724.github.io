b1db8c3ee02c5d53d9a4c06bd416e30dab0c2f68
github username <github email address>
2020-03-12 00:07:23 -0400

    adds more data for projects

--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const Project = (props) => {
-  let url = props.project.img
+
+  function renderHeroku(){
+    if (props.project.heroku) {
+      return <a href={props.project.heroku}><img className="icon" target="_blank" src="https://s3.us-east-2.amazonaws.com/eyg.portfolio/export.jpeg" /> </a>
+    }
+  }
+
   return (
       <div className="project">
         <h3> {props.project.title} </h3>
         <img className='appImage' src={props.project.img} />
         <p>{props.project.description} </p>
-         <a href='{props.project.link}'><img className="icon" src="https://s3.us-east-2.amazonaws.com/eyg.portfolio/github+icon.jpg" /> </a>
+        <a href={props.project.link}><img className="icon" target="_blank" src="https://s3.us-east-2.amazonaws.com/eyg.portfolio/github+icon.jpg" /> </a>
+        {renderHeroku()}
 
       </div>
   );
