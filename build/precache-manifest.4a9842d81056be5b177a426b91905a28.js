82f07595c2ef2ac5730edaf1040027fd3e98ef5b
github username <github email address>
2020-03-16 13:30:57 -0400

    deploys to github pages

---
+++ build/precache-manifest.4a9842d81056be5b177a426b91905a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec01fb4c9711f8c954f310f73dabc6da",
+    "url": "/erin-green.com/index.html"
+  },
+  {
+    "revision": "a126fef814cc347c245e",
+    "url": "/erin-green.com/static/css/main.6445daf4.chunk.css"
+  },
+  {
+    "revision": "3f445864c6415c38a299",
+    "url": "/erin-green.com/static/js/2.af0ca88e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/erin-green.com/static/js/2.af0ca88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a126fef814cc347c245e",
+    "url": "/erin-green.com/static/js/main.1562169d.chunk.js"
+  },
+  {
+    "revision": "b3552eabef3107abf50d",
+    "url": "/erin-green.com/static/js/runtime-main.2b631d62.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4b03f69811235dda5bdcb5f47d76166.js"
+  "/erin-green.com/precache-manifest.4a9842d81056be5b177a426b91905a28.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/erin-green.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
