59fe279b04f01f059ea3ce17f7ef46af3e042af9
github username <github email address>
2020-03-17 22:41:28 -0400

    Updates

---
+++ precache-manifest.28386e54fc14d7a2d2e7ae8696e32984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1b4221b4082ab565f52795a8501ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796141dd7593be6598b3",
+    "url": "/static/css/main.6445daf4.chunk.css"
+  },
+  {
+    "revision": "3f445864c6415c38a299",
+    "url": "/static/js/2.af0ca88e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.af0ca88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796141dd7593be6598b3",
+    "url": "/static/js/main.298ae507.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  }
+]);
